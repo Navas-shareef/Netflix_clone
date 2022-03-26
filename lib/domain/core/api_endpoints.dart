dbae714fb5dc3d69a24f0b3b70006f25fa7c1944
Navas-shareef <navasshareef@hamon.in>
2022-03-26 23:52:09 +0530

    added poster model to store poster image from api for download page

---
+++ lib/domain/core/api_endpoints.dart
@@ -0,0 +1,6 @@
+import 'package:netflixapp/core/strings.dart';
+import 'package:netflixapp/infrastructure/api_key.dart';
+
+class ApiEndPoints {
+  static const downloads = "$kBaseUrl/all/day?api_key=$apiKey";
+}
--- lib/presentation/new_and_hot/widgets/coming_soon.dart
+++ lib/presentation/new_and_hot/widgets/coming_soon.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflixapp/core/constant.dart';
 import 'package:netflixapp/presentation/home/widgets/custom_button_widget.dart';
+import 'package:netflixapp/presentation/widgets/vedeo_widget.dart';
 
 class ComingSoon extends StatelessWidget {
   const ComingSoon({
@@ -40,7 +41,7 @@ class ComingSoon extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const VedioWidget(),
+              const VedeoWidget(),
               Row(
                 children: const [
                   Text(
@@ -85,46 +86,6 @@ class ComingSoon extends StatelessWidget {
                     color: Colors.grey),
               ),
             ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class VedioWidget extends StatelessWidget {
-  const VedioWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          width: double.infinity,
-          height: 200,
-          child: Image.network(
-            "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202107/KGF-647x363.jpeg?0n7lFSoZP63T_VGoHMpIkMtOu9s6Jq_Z",
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned(
-          bottom: 10,
-          right: 10,
-          child: CircleAvatar(
-            backgroundColor: Colors.black.withOpacity(0.5),
-            radius: 22,
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.volume_off,
-                      size: 20,
-                    ))
-              ],
-            ),
           ),
         ),
       ],
